e01fbcbe3a61deb67bb59838670fa5b92e350883
Lord Sayur <umar_317@hotmail.com>
2021-01-17 22:01:55 +0800

    configure app and home component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template>
-  <router-view />
+  <div id="app">
+    <div class="content">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
+html,
+body {
+  width: 100%;
+  margin: 0;
+  box-sizing: content-box;
+  height: 100%;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background: $background;
+  font-size: 16px;
+  color: $secondary;
+}
+#app {
+  width: 100%;
+  height: 100%;
+}
+.content {
+  width: 95vw;
+  margin: auto;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>Home</div>
+  <div>
+    <h1>Home</h1>
+    <router-link to="/task">Tasks</router-link>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  setup() {},
 };
 </script>
+
+<style lang="scss" scoped>
+</style>
