fea1f4c20570d60a9805ad9dbdf133eded6d8788
Lord Sayur <umar_317@hotmail.com>
2021-01-17 22:03:44 +0800

    add task view component

---
+++ src/views/Task.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Tasks</h1>
+  <TasksList />
+</template>
+
+<script>
+import TasksList from "@/components/TasksList";
+
+export default {
+  name: "Task",
+  components: { TasksList },
+};
+</script>
+
+<style>
+</style>
